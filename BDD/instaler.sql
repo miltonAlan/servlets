70e85f2d7b585c190417fc2e71707ad2a4b44479
milton <miltonpaucar999@gmail.com>
2023-02-09 17:52:42 -0500

    mpaucar: feat: ajuste instalador BDD para agregar mas tablas

--- BDD/instaler.sql
+++ BDD/instaler.sql
@@ -2,9 +2,13 @@ create schema if not exists `servlets` default character set utf8 ;
 use `servlets` ;
 
 drop table if exists lugar_parqueadero;
+drop table if exists parqueaderos_disponibles;
+drop table if exists usuario;
+drop table if exists tipo_usuario;
+
 
 create table lugar_parqueadero(
-    id                              int(3) not null auto_increment,
+    id                              int  not null auto_increment,
     nombre_usuario                  varchar(100) default null,
     tipo_vehiculo                   varchar(100) default null,
     tiempo                          varchar(100) default null,
@@ -15,12 +19,27 @@ create table lugar_parqueadero(
     primary key (id)
 );
 
-
-drop table if exists parqueaderos_disponibles;
-
 create table parqueaderos_disponibles(
-    id                              int(3) not null auto_increment,
-    cantidad                        int,
+    id                              int  not null auto_increment,
+    cantidad_ocupados               int not null,
+    cantidad_disponibles            int not null,
     primary key (id)
 );
 
+create table tipo_usuario(
+    id                              int  not null auto_increment,
+    tipo                            varchar(100) default null,
+    primary key (id)
+);
+
+create table usuario(
+    id                              int  not null auto_increment,
+    nombre                          varchar(100) default null,
+    clave                           varchar(100) default null,
+    correo                          varchar(100) default null,
+    fecha_nacimiento                varchar(100) default null,
+    direccion                       varchar(100) default null,
+    tipo_usuario                    int  not null,
+    primary key (id),
+    constraint fk_tipo_usuario      foreign key (tipo_usuario) references tipo_usuario(id)
+);
